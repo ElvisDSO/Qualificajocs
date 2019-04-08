f26586ce88c6d4f145515718ddbf677ad813b454
ElvisDSO <33910948+ElvisDSO@users.noreply.github.com>
2019-04-09 00:52:28 +0200

    Tabla "generador_pendientes"
    Creación del script generador de los elementos e inserción en la basa de datos.

---
+++ Base de datos/Generador_Inserts/src/generador/generador_pendientes.java
@@ -0,0 +1,70 @@
+package generador;
+
+import java.io.*;
+
+public class generador_pendientes {
+	public static void main(String[] args) {
+
+		FileWriter fichero = null;
+		PrintWriter pw = null;
+		final int NUM_JUEGOS_POR_JUGADOR = 10;
+		final int NUM_JUGADORES_TOTALES = 500;
+
+		try {
+
+			fichero = new FileWriter("generador_pendientes.sql");
+			pw = new PrintWriter(fichero);
+
+			// final String[] rating = { "TODOS", "3", "7", "12", "16", "18" };
+
+			pw.println("INSERT INTO `lista_pendientes` (`ID_VIDEOJUEGO`, `ID_USUARIO`) VALUES ");
+			int contador = 1;
+			int id_jugador = 1;
+
+			for (int i = 1; i <= NUM_JUGADORES_TOTALES; i++) {
+				int[] juegos = new int[NUM_JUEGOS_POR_JUGADOR];
+
+				for (int j = 0; j < NUM_JUEGOS_POR_JUGADOR; j++) {
+					int a = getRandom.getRandomIntBetweenRange(1, 2000);
+					juegos[j] = a;
+				}
+
+				for (int m = 0; m < juegos.length; m++) {
+					for (int n = 0; n < juegos.length; n++) {
+						if ((m != n && (juegos[m] == juegos[n]))) {
+							juegos[m] = juegos[m] + 1;
+						}
+					}
+				}
+
+				for (int k = 0; k < juegos.length; k++) {
+					pw.print("('" + juegos[k] + "',");
+					pw.print("'" + id_jugador + "'");
+					pw.print(")");
+					if (i == NUM_JUGADORES_TOTALES && contador == NUM_JUEGOS_POR_JUGADOR) {
+						pw.println(";");
+					} else {
+						pw.println(",");
+					}
+					if (contador < NUM_JUEGOS_POR_JUGADOR) {
+						contador++;
+					} else {
+						id_jugador++;
+						contador = 1;
+					}
+				}
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (null != fichero)
+					fichero.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+}
