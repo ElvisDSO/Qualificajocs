9212cb98169f06e43651cddcc2792cfdf28b67e1
ElvisDSO <33910948+ElvisDSO@users.noreply.github.com>
2019-05-14 23:28:51 +0200

    Creación ficheros config e Index.php
    Creación de ficheros que conectan con la base de datos y un Index básico que inicializa dichos ficheros.

---
+++ Página Web/config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('SITE_URL','localhost');
+
+define('DB_NAME','qualificajocs');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_HOST','localhost');
+define('DB_PORT', 80);
+
+?>
